67d8139acc7e15a5b9435b02be820d4f45c06136
Tracy Lee <tleecoding@gmail.com>
2016-12-08 11:53:24 -0600

    icons added to vertical nav

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -14,3 +14,17 @@ $(function() {
         }
     });
 });
+
+function sideNav() {
+    if ($(window).width() < 769) {
+        $('.off-canvas-wrap').removeClass('move-right');
+        $('.left-off-canvas-toggle').show();
+    } else {
+        $('.off-canvas-wrap').addClass('move-right');
+        $('.left-off-canvas-toggle').hide();
+    }
+}
+
+$(window).resize(function() {
+    sideNav();
+});
