53edcd226f67d2d509736f5f17f0521d44c96e45
Tracy Lee <tleecoding@gmail.com>
2016-12-11 14:49:01 -0600

    created partials

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -14,16 +14,3 @@ $(function() {
         }
     });
 });
-
-function sideNav() {
-    if ($(window).width() < 769) {
-        $('.off-canvas-wrap').removeClass('move-right');
-        $('.left-off-canvas-toggle').show();
-    } else {
-        $('.off-canvas-wrap').addClass('move-right');
-        $('.left-off-canvas-toggle').hide();
-    }
-}
-$(window).resize(function() {
-    sideNav();
-});
