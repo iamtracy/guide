3cd5779002fb91a1fad8311dfed02f8579bb68ce
Tracy Lee <tleecoding@gmail.com>
2017-02-14 10:38:15 -0600

    specs link removed from bosy

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,34 +1,22 @@
+
+
 $(function() {
-    $('a[href^="#"]:not([href^="#panel"])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: target.offset().top - 53
-                }, 750);
-                return false;
-            }
-        }
-    });
-});
-
-function sideNav() {
-    if ($(window).width() < 769) {
-        $('.off-canvas-wrap').removeClass('move-right');
-        $('.left-off-canvas-toggle').show();
-    } else {
-        $('.off-canvas-wrap').addClass('move-right');
-        $('.left-off-canvas-toggle').hide();
+  $('a[href^="#"]:not([href^="#panel"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 53
+        }, 750);
+        return false;
+      }
     }
-}
-
-$(window).resize(function() {
-    sideNav();
+  });
 });
 
 $('[data-toggle="dropdown"]').click(function() {
-    $('[data-toggle="dropdown-tabs-content"]').toggleClass('hide');
+  $('[data-toggle="dropdown-tabs-content"]').toggleClass('hide');
 })
 
 /*Dynamically Sets Year in Footer Copyright Section*/
@@ -36,9 +24,10 @@ var year = new Date().getFullYear();
 document.getElementById('dynamicYear').innerHTML = year;
 
 function piperfy() {
-    let piper = $('.fa').not('[data-toggle="dropdown"]');
-    let pipeArray = piper.toArray();
-    pipeArray.forEach(item => item.classList = 'fa fa-pied-piper-alt');
+  let piper = $('.fa')
+    .not('[data-toggle="dropdown"]')
+    .toArray();
+  piper.map(item => item.classList = 'fa fa-pied-piper-alt');
 }
 
 $(document).foundation();
